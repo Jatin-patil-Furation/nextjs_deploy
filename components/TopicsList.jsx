1541684e476386358e3760d54569494c45ca88e1
Jatin Patil <jatin.patil@furation.tech>
2023-11-16 13:06:25 +0530

    first commit

--- components/TopicsList.jsx
+++ components/TopicsList.jsx
@@ -1,41 +1,49 @@
+
+"use client";
+import React, { useEffect, useState } from "react";
 import Link from "next/link";
 import RemoveBtn from "./RemoveBtn";
 import { HiPencilAlt } from "react-icons/hi";
 
-const getTopics = async () => {
-  try {
-    const res = await fetch("http://localhost:3000/api/topics", {
-      cache: "no-store",
-    });
+const TopicsList = () => {
+  const [topics, setTopics] = useState([]);
 
-    if (!res.ok) {
-      throw new Error("Failed to fetch topics");
-    }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch("http://localhost:3000/api/topics", {
+          cache: "no-store",
+        });
 
-    return res.json();
-  } catch (error) {
-    console.log("Error loading topics: ", error);
-  }
-};
+        if (!res.ok) {
+          throw new Error("Failed to fetch topics");
+        }
 
-export default async function TopicsList() {
-  const { topics } = await getTopics();
+        const data = await res.json();
+        setTopics(data.topics);
+      } catch (error) {
+        console.log("Error loading topics: ", error);
+      }
+    };
+
+    fetchData();
+  }, []); // Empty dependency array ensures that this effect runs once after the initial render
 
   return (
     <>
       {topics.map((t) => (
         <div
-          key={t._id}
+          key={t?._id}
           className="p-4 border border-slate-300 my-3 flex justify-between gap-5 items-start"
         >
           <div>
-            <h2 className="font-bold text-2xl">{t.title}</h2>
-            <div>{t.description}</div>
+            <h2 className="font-bold text-2xl">{t?.title}</h2>
+            <div>{t?.description}</div>
           </div>
 
           <div className="flex gap-2">
-            <RemoveBtn id={t._id} />
-            <Link href={`/editTopic/${t._id}`}>
+            <RemoveBtn id={t?._id} />
+            <Link href={`/editTopic/${t?._id}`}>
               <HiPencilAlt size={24} />
             </Link>
           </div>
@@ -43,4 +51,6 @@ export default async function TopicsList() {
       ))}
     </>
   );
-}
+};
+
+export default TopicsList;
